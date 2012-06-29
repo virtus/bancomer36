29f8a4f5cefc4ed6a25daabe00413a2a65078b54
Hector Sansores <hector.sansores@virtus.com.mx>
2012-06-28 23:01:07 -0500

    Implement complete algorithm

--- lib/bancomer36.rb
+++ lib/bancomer36.rb
@@ -4,6 +4,7 @@ module Bancomer36
   class CheckDigit
 
     # Replaces letters in input string with numbers
+    # input is a string containing letters from A to Z and numbers from 0 to 9
     def letter_sub(input)
       hash = {
         'A' => '1', 'B' => '2', 'C' => '3', 'D' => '4', 'E' => '5', 'F' => '6',
@@ -18,10 +19,83 @@ module Bancomer36
       output
     end
 
-    # Coverts a string containing numbers to an array of integers
+    # Converts a string containing numbers to an array of integers
     def to_int_array(input)
       input.chars.to_a.collect { |x| x.to_i }
     end
 
+    # Multiplies by 2 the elements in even positions (0, 2, 4...)
+    # input is a array of integers with values ranging from 0 to 9
+    def multiply_by_2(input)
+      output = []
+      input.each_with_index do |value, index|
+        if index % 2 == 0
+          output << value * 2
+        else
+          output << value
+        end
+      end
+      output
+    end
+
+    # Sums units and tens for each element greater than 9
+    # input is a array of integers with values ranging from 0 to 18
+    def sums_units_and_tens(input)
+      hash = {
+        10 => 1, 11 => 2, 12 => 3, 13 => 4, 14 => 5,
+        15 => 6, 16 => 7, 17 => 8, 18 => 9
+      }
+      output = []
+      input.each do |x|
+        if x > 9
+          output << hash[x]
+        else
+          output << x
+        end
+      end
+      output
+    end
+
+    # Sums all elements
+    # input is a array of integers with values ranging from 0 to 9
+    def sums_all_elements(input)
+      input.inject(:+)
+    end
+
+    # Substracts from the next multiple of ten
+    # input is an integer
+    def substracts_from_next_ten(input)
+      output = 10 - input % 10
+      output = 0 if output == 10
+      output
+    end
+
+    # Returns the check digit
+    # input is a string containing letters from A to Z and numbers from 0 to 9
+    def check_digit(input)
+      input = letter_sub(input)
+      input = to_int_array(input)
+      input = multiply_by_2(input)
+      input = sums_units_and_tens(input)
+      input = sums_all_elements(input)
+      input = substracts_from_next_ten(input)
+      input
+    end
+
+    # Appends the check digit
+    # input is a string containing letters from A to Z and numbers from 0 to 9
+    def append_check_digit(input)
+      input + check_digit(input).to_s
+    end
+
+    # Validates the check digit
+    # input is a string containing letters from A to Z and numbers from 0 to 9
+    # and a check digit at the end
+    def check_digit_valid?(input)
+      digit = input[-1].to_i
+      input = input[0..input.length-1]
+      input = input
+      check_digit(input) == digit
+    end
   end
 end
--- spec/bancomer36_spec.rb
+++ spec/bancomer36_spec.rb
@@ -1,29 +1,50 @@
 require 'bancomer36'
 
 describe Bancomer36::CheckDigit do
+  before :each do
+    @cd = Bancomer36::CheckDigit.new
+  end
+
   context 'internal methods' do
     it 'replaces letters with numbers' do
-      cd = Bancomer36::CheckDigit.new
-      cd.letter_sub('ABCDEFGHI').should == '123456789'
-      cd.letter_sub('JKLMNOPQR').should == '123456789'
-      cd.letter_sub('STUVWXYZ' ).should == '12345678'
+      @cd.letter_sub('ABCDEFGHI').should == '123456789'
+      @cd.letter_sub('JKLMNOPQR').should == '123456789'
+      @cd.letter_sub('STUVWXYZ' ).should == '12345678'
     end
     it 'leaves numbers unchanged' do
-      cd = Bancomer36::CheckDigit.new
-      cd.letter_sub('0123456789').should == '0123456789'
+      @cd.letter_sub('0123456789').should == '0123456789'
     end
     it 'converts a string to an array of ints' do
-      cd = Bancomer36::CheckDigit.new
-      cd.to_int_array('0123456789').should == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      @cd.to_int_array('0123456789').should == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     end
-    it 'multiplies by 2 the elements in even positions (0, 2, 4...)'
-    it 'sums units and tens for each element greater than 9'
-    it 'sums all the elements'
-    it 'substracts from the next multiple of ten'
+    it 'multiplies by 2 the elements in even positions (0, 2, 4...)' do
+      input = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      @cd.multiply_by_2(input).should == [0, 1, 4, 3, 8, 5, 12, 7, 16, 9]
+    end
+    it 'sums units and tens for each element greater than 9' do
+      input  = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+      output = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      @cd.sums_units_and_tens(input).should == output
+    end
+    it 'sums all the elements' do
+      input  = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      @cd.sums_all_elements(input).should == 45
+    end
+    it 'substracts from the next multiple of ten' do
+      @cd.substracts_from_next_ten(45).should == 5
+      @cd.substracts_from_next_ten(30).should == 0
+    end
   end
   context 'user methods' do
-    it 'returns the check digit'
-    it 'append the check digit'
-    it 'verifies the check digit'
+    it 'returns the check digit' do
+      @cd.check_digit('ABCDEF01235').should == 0
+    end
+    it 'appends the check digit' do
+      @cd.append_check_digit('ABCDEF01235').should == 'ABCDEF012350'
+    end
+    it 'validates the check digit' do
+      @cd.check_digit_valid?('ABCDEF012350').should be_true
+      @cd.check_digit_valid?('ABCDEF012351').should be_false
+    end
   end
 end
